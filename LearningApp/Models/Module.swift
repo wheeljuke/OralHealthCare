a7ca9cf7ba3c045158424ef9d81991b15ebd17f5
Stephen200274 <stephen200274@gmail.com>
2022-03-24 06:31:28 +0800

    -add assets -add dataModels -add json and style parsing

---
+++ LearningApp/Models/Module.swift
@@ -0,0 +1,51 @@
+//
+//  Module.swift
+//  LearningApp
+//
+//  Created by 洪偉倫 on 2022/3/24.
+//
+
+import Foundation
+
+struct Module: Decodable, Identifiable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+}
+
+struct Content: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable, Identifiable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable, Identifiable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+    
+}
--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,56 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // MARK: Parse the jsonData
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                
+                let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+                
+                self.modules = modules
+            }
+            
+            catch {
+                print("Can't Decode jsonData")
+            }
+        }
+        catch {
+            
+            print("Error fetching the jsonData from URL")
+        }
+        
+        // MARK: Parse the StyleData
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+        }
+        
+        catch {
+            
+            print("Error fetching the htmlData from URL")
+        }
+    }
 }
+
