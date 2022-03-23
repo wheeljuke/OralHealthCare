7623b34daa625473613d04a3ed6bd191b5bbbb35
Stephen200274 <stephen200274@gmail.com>
2022-03-24 05:44:53 +0800

    Initial Commit

---
+++ LearningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by 洪偉倫 on 2022/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by 洪偉倫 on 2022/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
