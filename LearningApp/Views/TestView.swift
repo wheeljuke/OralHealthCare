7fb5880d1b70dd5ef8a095a6ab6e1470c03991d8
Stephen200274 <stephen200274@gmail.com>
2022-04-05 06:14:03 +0800

    - Lesson 10 - add answer buttons in TestView - add submit button in TestView

--- LearningApp/Views/TestView.swift
+++ LearningApp/Views/TestView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex:Int?
+    @State var submitted = false
+    @State var numQuestionCorrect = 0
     
     var body: some View {
         
         if model.currentQuestion != nil {
-            VStack {
+            VStack(alignment: .leading) {
                 
                 // Question Number
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
@@ -29,6 +32,87 @@ struct TestView: View {
             }
             .navigationTitle("\(model.currentModule?.category ?? "") Test")
             .padding(.horizontal)
+            
+            // MARK: Answers selection
+            ScrollView {
+                
+                VStack {
+                    
+                    ForEach(0..<model.currentQuestion!.answers.count) { index in
+                        
+                        Button {
+                            
+                            selectedAnswerIndex = index
+                        } label: {
+                            ZStack {
+                                
+                                // Answer hasn't been submitted
+                                if submitted == false {
+                                    RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                }
+                                
+                                // Answer has been submitted
+                                else {
+                                    
+                                    // User has selected the corrent answer change the correct answer to green
+                                    if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex {
+
+                                        RectangleCard(color: .green)
+                                    }
+                                    
+                                    // User has selected the wrong answer change the wrong answer to red
+                                    else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                        
+                                        RectangleCard(color: .red)
+                                    }
+                                    
+                                    // User has selected the wrong answer change the correct answer to green
+                                    else if index != selectedAnswerIndex && index == model.currentQuestion!.correctIndex {
+                                        
+                                        RectangleCard(color: .green)
+                                    }
+                                    
+                                    else {
+                                        RectangleCard(color: .white)
+                                    }
+                                }
+                                
+                                Text(model.currentQuestion!.answers[index])
+                            }
+                        }
+                        .disabled(submitted)
+                        .opacity(1)
+                    }
+                }
+                .padding()
+                .accentColor(.black)
+                
+                // MARK: Submit Button
+                Button {
+                    
+                    // Mark the state to be submitted
+                    submitted = true
+                    
+                    //Check the answers and record the number of correct questions
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        numQuestionCorrect += 1
+                    }
+                    
+                } label: {
+                    
+                    ZStack {
+                        
+                        RectangleCard(color: .green)
+                        
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                }
+                // Make sure that when no answers were selected it can't be submitted
+                .disabled(selectedAnswerIndex == nil || submitted == true)
+            }
         }
         
         else {
